88e089211d2975d26ffd671d9a6f9872c83bc45c
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-21 22:54:54 -0700

    updated files new repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fitness Tracker
+
+A daily exercise tracker app. Allows user to add new workouts or update existing workouts with type of exercise, weight, reps, etc.
+
+Technologies used:
+
+- Express
+- Mongo DB
+- Node
+- Mongoose
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 8080;
+var db = require("./Models");
+
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/custommethoddb",
+  { useNewUrlParser: true }
+);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
